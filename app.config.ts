564424dff8e2adfb52e7d82bbb793d84749d1dc5
ayaelbadrawey <ayaelbadrawey@gmail.com>
2024-06-04 22:32:09 +0300

    add layout

---
+++ app.config.ts
@@ -0,0 +1,6 @@
+export default defineAppConfig({
+  nuxtIcon: {
+    size: "25px", // default <Icon> size applied
+    class: "icon", // default <Icon> class applied
+  },
+});
---
+++ layouts/default.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="mx-10">
+        <div class="flex flex-row justify-between my-5">
+            <div class="flex flex-col w-fit">
+                <span class="border-b-2 border-gray-200 font-bold text-rose-700 text-lg">TOYOTA</span>
+                <span class="text-gray-600 pt-2">TOYOTA MATERIAL HANDLING INTERNATIONAL</span>
+            </div>
+            <div class="flex flex-row justify-between items-center">
+                <nuxt-link class="px-5 font-semibold" to="/products">PRODUCTS</nuxt-link>
+                <nuxt-link class="px-5 font-semibold" to="/">SERVICES</nuxt-link>
+                <nuxt-link class="px-5 font-semibold" to="/">TRAINING</nuxt-link>
+                <nuxt-link class="px-5 font-semibold" to="/">ABOUT US</nuxt-link>
+                <span class="px-5">
+                    <Icon name="eva:search-fill" color="black" />
+                </span>
+                <button class="px-5 mx-5 rounded-2xl py-2 bg-red-700 text-white">GET QUOTE</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped></style>
