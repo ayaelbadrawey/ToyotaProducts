e145c66cee339847d3d512381b714cec5c26f0f6
ayaelbadrawey <ayaelbadrawey@gmail.com>
2024-06-05 23:42:52 +0300

    select filter component

---
+++ components/SelectFilter.vue
@@ -0,0 +1,38 @@
+<<template>
+    <div class="bg-gray-100 rounded-md w-fit p-5 pb-10 border border-gray-200">
+        <span class>Filter Name</span>
+        <div class="flex flex-row items-baseline mt-5">
+            <div class="flex flex-col text-center">
+                <button class="bg-gray-200 rounded-full p-5"></button>
+                <span>Level</span>
+            </div>
+            <span class="border-b-2 px-4"></span>
+            <div class="flex flex-col text-center">
+                <button class="bg-gray-200 rounded-full p-5"></button>
+                <span>Level</span>
+            </div>
+            <span class="border-b-2 px-4"></span>
+            <div class="flex flex-col text-center">
+                <button class="bg-gray-200 rounded-full p-5"></button>
+                <span>Level</span>
+            </div>
+            <span class="border-b-2 px-4"></span>
+            <div class="flex flex-col text-center">
+                <button class="bg-gray-200 rounded-full p-5"></button>
+                <span>Level</span>
+            </div>
+            <span class="border-b-2 px-4"></span>
+            <div class="flex flex-col text-center">
+                <button class="bg-gray-200 rounded-full p-5"></button>
+                <span>Level</span>
+            </div>
+
+        </div>
+    </div>
+</template>
+
+    <script setup>
+
+</script>
+
+    <style></style>
--- pages/services/index.vue
+++ pages/services/index.vue
@@ -3,6 +3,11 @@
         <div class="flex justify-center flex-row">
             <span class="font-bold text-2xl border-b border-red-700">RANGES</span>
         </div>
+        <div class="mt-10 flex flex-col md:flex-row gap-4">
+            <SelectFilter></SelectFilter>
+            <SelectFilter></SelectFilter>
+
+        </div>
     </div>
 </template>
 <script setup>
