d6d8f6a8cec14fb2fd9250fa0235b5bb406d8e94
ayaelbadrawey <ayaelbadrawey@gmail.com>
2024-06-05 21:50:09 +0300

    edit layout style

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,15 +5,15 @@
                 <span class="border-b-2 border-gray-200 font-bold text-rose-700 text-lg">TOYOTA</span>
                 <span class="text-gray-600 pt-2">TOYOTA MATERIAL HANDLING INTERNATIONAL</span>
             </div>
-            <div class="flex flex-row justify-between items-center">
-                <nuxt-link class="px-5 font-semibold" to="/products">PRODUCTS</nuxt-link>
-                <nuxt-link class="px-5 font-semibold" to="/">SERVICES</nuxt-link>
-                <nuxt-link class="px-5 font-semibold" to="/">TRAINING</nuxt-link>
-                <nuxt-link class="px-5 font-semibold" to="/">ABOUT US</nuxt-link>
-                <span class="px-5">
+            <div class="flex flex-row justify-between items-center gap-10 font-semibold">
+                <nuxt-link to="/products">PRODUCTS</nuxt-link>
+                <nuxt-link to="/">SERVICES</nuxt-link>
+                <nuxt-link to="/">TRAINING</nuxt-link>
+                <nuxt-link to="/">ABOUT US</nuxt-link>
+                <span>
                     <Icon name="eva:search-fill" color="black" />
                 </span>
-                <button class="px-5 mx-5 rounded-2xl py-2 bg-red-700 text-white">GET QUOTE</button>
+                <button class="px-5 rounded-2xl py-2 bg-red-700 text-white">GET QUOTE</button>
             </div>
         </div>
     </div>
